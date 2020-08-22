29a116fbcd07fd29e071f29f999cfc2d4f941610
buraksekili <buraksekili@gmail.com>
2020-08-22 19:47:17 +0300

    Removed redundant requirements

--- models/Document.js
+++ models/Document.js
@@ -1,7 +1,4 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const Schema = mongoose.Schema;
 
 const DocumentSchema = new Schema({
